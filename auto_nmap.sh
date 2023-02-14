ab4656bf3433c2490e39c49a59f33edb11fee143
Dogru-Isim <96004353+Dogru-Isim@users.noreply.github.com>
2023-02-14 13:11:54 +0100

    Update auto_nmap.sh

--- auto_nmap.sh
+++ auto_nmap.sh
@@ -3,7 +3,6 @@
 target=$1
 nmap_dir=${PWD}/nmap/
 
-
 if [ -z $target ]; then
   echo "Provide a target"
   exit
@@ -11,23 +10,29 @@ elif [ ! -d $nmap_dir ]; then
   mkdir ${PWD}/nmap/
 fi
 
-echo "1. Starting probing top ports"
+# PROBING TOP PORTS
+echo "Starting probing top ports"
 open_ports=$(nmap -T4 $target | grep open | awk -F '/' '{print $1}' | tr "\n" "," | sed 's/,$//g')
 # format: 21,22,80
 
-echo "2. Starting scanning top ports"
+# SCANNING TOP PORTS
+echo "Starting scanning top ports"
 nmap -T4 $target -p $open_ports -sC -sV -oN ${PWD}/nmap/initial.lst >/dev/null
 
-echo "3. Starting allport scan"
-all_ports=$(nmap -T4 $target -p- -oN ${PWD}/nmap/allports.lst | grep open | awk -F '/' '{print $1}' | tr "\n" "," | sed 's/,$//g')
+# PROBING ALL PORTS
+echo "Starting allports scan"
+nmap -v -T4 $target -p- -oN ${PWD}/nmap/allports.lst
+all_ports=$(cat ${PWD}/nmap/allports.lst | grep open | awk -F '/' '{print $1}' | tr "\n" "," | sed 's/,$//g')
 
+# CHECK IF MORE PORTS ARE FOUND
 if [[ $open_ports -eq $all_ports ]]; then
   echo "No extra ports were found!"
   rm nmap/allports.lst
   exit 0
 fi
 
-echo "4. Starting scanning all ports"
+# IF MORE PORTS ARE FOUND, SCAN THEM
+echo "Starting scanning all ports"
 nmap -T4 $target -p $all_ports -sC -sV -oN ${PWD}/nmap/comprehensive.lst >/dev/null
 
 exit
